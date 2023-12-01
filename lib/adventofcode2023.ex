5a8d37974dd2f47376f90a85c6da70732afcc21e
Tim Arbuckle <timarbuckle@gmail.com>
2023-12-01 09:35:24 -0800

    initialize project

---
+++ lib/adventofcode2023.ex
@@ -0,0 +1,18 @@
+defmodule Adventofcode2023 do
+  @moduledoc """
+  Documentation for `Adventofcode2023`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adventofcode2023.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/adventofcode2023_test.exs
@@ -0,0 +1,8 @@
+defmodule Adventofcode2023Test do
+  use ExUnit.Case
+  doctest Adventofcode2023
+
+  test "greets the world" do
+    assert Adventofcode2023.hello() == :world
+  end
+end
