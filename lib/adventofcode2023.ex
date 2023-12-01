9fa60a6f00597f7464de47c3ffaeafca117dbb1d
Tim Arbuckle <timarbuckle@gmail.com>
2023-12-01 11:06:16 -0800

    working solution for day 1]

--- lib/adventofcode2023.ex
+++ lib/adventofcode2023.ex
@@ -15,4 +15,27 @@ defmodule Adventofcode2023 do
   def hello do
     :world
   end
+
+  def calibrate_trebuchet(cali_info) do
+    cali_info
+    |> String.split("\n")
+    |> Enum.map(fn x -> retrieve_cali_value(x) end)
+    |> Enum.sum()
+  end
+
+  defp retrieve_cali_value(row) do
+    #IO.puts("-----")
+    #IO.puts("row #{row}")
+    allints = String.replace(row, ~r/[^\d+]/, "")
+    #IO.puts("allints #{allints}")
+    case String.length(allints) do
+      0 -> 0
+      _ ->
+        x = String.to_integer(String.at(allints, 0))
+        y = String.to_integer(String.at(allints, -1))
+        #IO.puts("sum #{10*x + y}")
+        10 * x + y
+    end
+  end
+
 end
--- test/adventofcode2023_test.exs
+++ test/adventofcode2023_test.exs
@@ -1,8 +1,19 @@
 defmodule Adventofcode2023Test do
   use ExUnit.Case
+
   doctest Adventofcode2023
 
   test "greets the world" do
     assert Adventofcode2023.hello() == :world
+  end
+
+  test "Day 1  Trebuchet?!" do
+    calibration_info = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"
+    assert Adventofcode2023.calibrate_trebuchet(calibration_info) == 142
+  end
+
+  test "Day 1 Puzzle" do
+    {:ok, calibration_info} = File.read("test/day1_test.txt")
+    assert Adventofcode2023.calibrate_trebuchet(calibration_info) == 54081
   end
 end
